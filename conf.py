b1abab695c7cb11bd9ef1b0570f0c9edfa2f3053
IgoyAI <aagdeyogipramana@mail.ugm.ac.id>
2025-04-06 20:13:43 +0800

    update

--- conf.py
+++ conf.py
@@ -26,4 +26,4 @@ language = 'english'
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ['_static']
+html_static_path = ['https://gmsaintek.github.io/doc-ref/_static']
