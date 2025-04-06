4c55e1c193de07516a9a8c7536d7461e8c61962b
IgoyAI <aagdeyogipramana@mail.ugm.ac.id>
2025-04-06 19:18:53 +0800

    Fix folders

--- conf.py
+++ conf.py
@@ -3,26 +3,27 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import sphinx_rtd_theme
 
+# If your project modules are in a different directory, you might need to
+# add that directory to sys.path here. For example:
+# sys.path.insert(0, os.path.abspath('../src'))
+
+# -- Project information -----------------------------------------------------
 project = 'gm-docs-ref'
 copyright = '2025, GM_TEAM, Yogi P.'
 author = 'GM_TEAM, Yogi P.'
 release = '0.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = []
-
 language = 'english'
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
